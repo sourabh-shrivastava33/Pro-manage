160e4836ea9a6fbff6a55214bbb8b8c2452f165c
sourabh shrivastava <sourabhshrivastava3011@gmail.com>
2024-03-02 13:32:55 +0530

    Clean up the redundant api call in login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import banner from "../assets/images/banner.png";
 import { RegisterLoginWrapper } from "../assets/styled-components/RegisterLoginWrapper";
 import LoginForm from "../components/LoginForm";
-import { useGetTaskQuery } from "../slices/taskApiSlice";
 
 const LoginPage = () => {
-  const { data, isError, isLoading } = useGetTaskQuery(
-    "65d71c6acaaec83ee6bad015"
-  );
-  console.log(data, isLoading);
   return (
     <RegisterLoginWrapper>
       <div className="banner-container">
